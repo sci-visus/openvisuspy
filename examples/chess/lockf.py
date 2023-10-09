c43d282891fc051ee633cd8a4303272b4a5d0874
gtarcea <gtarcea@umich.edu>
2023-10-09 12:30:01 -0400

    Example code to show locking and checking for lock

---
+++ examples/chess/lockf.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import fcntl
+import sys
+import time
+
+if __name__ == "__main__":
+    handle = open("/tmp/lockfile", "w+")
+    fd = handle.fileno()
+    try:
+        fcntl.flock(fd, fcntl.LOCK_EX)
+    except OSError as e:
+        print(f"Unable to lock file {e.errno}")
+        sys.exit(1)
+
+    print("lockf: Lock acquired")
+    time.sleep(1000)
