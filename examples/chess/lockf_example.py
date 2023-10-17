f1c68397b0a1ca54e6aacac9efbca9c2c318ad05
Giorgio Scorzelli <scrgiorgio@gmail.com>
2023-10-17 19:53:13 +0200

    fixing tabs vs spaces (use tabs)

--- examples/chess/lockf_example.py
+++ examples/chess/lockf_example.py
@@ -5,13 +5,13 @@ import sys
 import time
 
 if __name__ == "__main__":
-    handle = open("/tmp/lockfile", "w+")
-    fd = handle.fileno()
-    try:
-        fcntl.flock(fd, fcntl.LOCK_EX)
-    except OSError as e:
-        print(f"Unable to lock file {e.errno}")
-        sys.exit(1)
+	handle = open("/tmp/lockfile", "w+")
+	fd = handle.fileno()
+	try:
+		fcntl.flock(fd, fcntl.LOCK_EX)
+	except OSError as e:
+		print(f"Unable to lock file {e.errno}")
+		sys.exit(1)
 
-    print("lockf: Lock acquired")
-    time.sleep(1000)
+	print("lockf: Lock acquired")
+	time.sleep(1000)
