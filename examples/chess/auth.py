21f2d337055f4a3384978e68a9dbab559e24cf92
Giorgio Scorzelli <scrgiorgio@gmail.com>
2023-10-24 02:19:59 -0400

    fixing nginx reverse proxy

---
+++ examples/chess/auth.py
@@ -0,0 +1,55 @@
+"""
+auth.py, when hooked into Bokeh server, will require Active Directory login by users.
+Currently, it expects the environment variables AD_SERVER and AD_DOMAIN to be set correctly.
+
+This module is meant to be specified as argument to a bokeh server.
+bokeh serve --auth-module=auth.py --cookie-secret YOURSECRETHERE [...]
+"""
+import os
+
+import easyad
+from tornado.escape import json_decode, url_escape, json_encode
+from tornado.web import RequestHandler
+
+APP_URL    = "/app"
+login_url  = f"{APP_URL}/login"
+logout_url = f"{APP_URL}/logout"
+
+# //////////////////////////////////////////////////////
+def get_user(request_handler):
+	user_json = request_handler.get_secure_cookie("user")
+	if user_json:
+		return json_decode(user_json)
+	else:
+		return None
+
+# //////////////////////////////////////////////////////
+class LogoutHandler(RequestHandler):
+	def get(self):
+		self.clear_cookie("user")
+		self.redirect(login_url)
+
+
+# //////////////////////////////////////////////////////
+class LoginHandler(RequestHandler):
+	"""
+	The handler for logins. Bokeh promises to include a route to this handler
+	"""
+
+	def get(self):
+		self.render("chess_login.html")
+
+	def post(self):
+		self.ad = easyad.EasyAD({
+			'AD_SERVER': os.environ.get('AD_SERVER', None),
+			'AD_DOMAIN': os.environ.get('AD_DOMAIN', None)
+		})
+		username = self.get_argument("username", "")
+		password = self.get_argument("password", "")
+		is_authorised = self.ad.authenticate_user(username, password, json_safe=True)
+		if is_authorised and username:
+			self.set_secure_cookie("user", json_encode(username))
+			self.redirect(APP_URL)
+		else:
+			self.redirect(login_url + "?error=" + url_escape("Login incorrect."))
+			self.clear_cookie("user")
