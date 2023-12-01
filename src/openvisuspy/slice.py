9df95db4faa5e2149ae7c1e608352ce42bc7f8f6
Giorgio Scorzelli <scrgiorgio@gmail.com>
2023-12-01 10:55:29 -0500

    working...

--- src/openvisuspy/slice.py
+++ src/openvisuspy/slice.py
@@ -383,7 +383,6 @@ class Slice:
 		d["timestep"       ] = d.get("timestep", self.db.getTimesteps()[0])
 		d['view-dep'       ] = d.get('view-dep', True)
 		d['resolution'     ] = d.get("resolution", self.db.getMaxResolution() - 6)
-		d["direction"      ] = d.get("direction",2)
 		field = self.db.getField(d.get("field", self.db.getField().name))
 		dtype_range = field.getDTypeRange()
 		dtype_vmin, dtype_vmax = dtype_range.From, dtype_range.To
@@ -397,7 +396,7 @@ class Slice:
 			self.setTimestepDelta(int(d["timestep-delta" ]))
 			self.setTimestep(int(d["timestep"]))
 			self.setField(field.name)
-			self.setDirection(int(d["direction"]))
+			self.setDirection(2)
 			self.setViewDependent(bool(d['view-dep']))
 			self.setResolution(int(d['resolution']))
 			self.setPalette(palette)
