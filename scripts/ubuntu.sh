87b33ff62ccd83640db180dc454c913548ac286b
Giorgio Scorzelli <scrgiorgio@gmail.com>
2024-05-21 09:23:07 +0200

    New tag (1.0.50)

--- scripts/ubuntu.sh
+++ scripts/ubuntu.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+# disable keyring
 export PYTHON_KEYRING_BACKEND="keyring.backends.null.Keyring"
+python3 -m keyring --disable
 
 python3 -m pip install --upgrade pip
 python3 -m pip install hatch build keyring
 
 # will get the version from `pyproject.toml`
 python3 -m build . --wheel
+unzip -l dist/*.whl
 
 # get OpenVisus version
 python3 -m pip install --upgrade OpenVisusNoGui 
@@ -17,7 +20,7 @@ GIT_TAG=`git describe --tags --exact-match 2>/dev/null || true`
 if [[ "${GIT_TAG}" != "" ]] ; then
 
   # publish to PyPi
-  hatch publish --yes --no-prompt --user ${PYPI_USERNAME} --client-key ${PYPI_TOKEN}
+  hatch publish --yes --no-prompt --user "${PYPI_USERNAME}" --auth "${PYPI_TOKEN}"
 
   # publish to DockerHub
   docker build --build-arg="OPENVISUS_VERSION=${OPENVISUS_VERSION}" --build-arg="GIT_TAG=${GIT_TAG}" --tag nsdf/openvisuspy:${GIT_TAG} ./
